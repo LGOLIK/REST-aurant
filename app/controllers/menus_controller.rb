1f31f7a75bc530acbc5326ce440ae6daea8696a0
LGOLIK <golikster@gmail.com>
2016-03-27 17:17:21 -0400

    Menu page fully functional - can view all, add, delete

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -18,16 +18,29 @@ class MenusController < ApplicationController
     end
   end
 
+  # edit to bring up the edit menu form
+  def edit
+    @menu = Menu.find(params[:id])
+  end
+
+  # update to actually perform the update action
+  def update
+    @menu = Menu.find(params[:id])
+    @menu.update(menu_params)
+    redirect_to menus_path
+  end
+
   def destroy
-     @menu = Menu.find(params[:id])
-     @menu.destroy
-     redirect_to menus_path
+    @menu = Menu.find(params[:id])
+    @menu.destroy
+    redirect_to menus_path
   end
 
 
   private
 
   def menu_params
+    # binding.pry
     params.require(:menu).permit(:name, :cuisine, :price)
   end
 
